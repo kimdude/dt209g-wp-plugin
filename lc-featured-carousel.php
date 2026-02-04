f42082e83fa2950513d37172534b02610e4c360c
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-02-04 12:37:17 +0100

    Lagt till html och css.

--- lc-featured-carousel.php
+++ lc-featured-carousel.php
@@ -6,4 +6,65 @@
  * Author: Kim Dudenhöfer
  */
 
-?>
+add_action("init", "lc_register_shortcodes");
+
+//Registering shortcode for carousel
+function lc_register_shortcodes() {
+    add_shortcode("carousel", "lc_carousel");
+}
+
+//Creating carousel with featured posts
+function lc_carousel() {
+
+    //Queueing styling
+    wp_enqueue_style(
+        "lc-carousel-style",
+        plugin_dir_url(__FILE__) . "style.css",
+        [],
+        "1.0"
+    );
+
+
+    ob_start();
+    ?>
+
+    <div class="preview-services">
+
+        <!-- Getting products that should be displayed on homepage -->
+        <?php
+        $products = new WP_Query([  
+            "post_type" => "product",
+            "meta_key" => "display_on_homepage",
+            "meta_value" => "Yes",
+            "posts_per_page" => 10
+        ]);
+
+        if($products->have_posts()) {
+            while($products->have_posts()) {
+                $products->the_post();
+                ?>
+
+                    <a href="<?php the_permalink(); ?>">
+                        <article>
+                            <?php
+                            if(has_post_thumbnail()) {
+                                the_post_thumbnail("product-thumbnail");
+                            }
+                            ?>
+                            <div>
+                                <h3><?php the_title(); ?></h3>
+                            </div>
+                        </article>
+                    </a>
+                <?php
+            }
+            wp_reset_postdata();
+        }
+        ?>
+
+    </div>
+
+    <?php
+    return ob_get_clean();
+}
+
